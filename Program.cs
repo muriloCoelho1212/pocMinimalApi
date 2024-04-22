1606425d5cca7a51afa8bb450b3086ecc6d1f2d1
Murilo <m.coelhos1212@gmail.com>
2024-04-22 17:19:41 -0300

    feat: initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+var app = builder.Build();
+
+app.MapGet("/todoitems", () => async (TodoDb db) => 
+    await db.Todos.ToListAsync());
+
+app.MapGet("/todoitems/complete", async (TodoDb db) =>  
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound()
+);
+
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) =>
+{
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+})
+
+app.Run();
