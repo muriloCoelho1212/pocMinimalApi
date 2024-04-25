45cebd459c9de6944ac1a797a91cef7a10746377
Murilo <m.coelhos1212@gmail.com>
2024-04-25 08:29:51 -0300

    feat: create endpoints to make crud operations

--- Program.cs
+++ Program.cs
@@ -9,25 +9,52 @@ builder.Services.AddEntityFrameworkNpgsql().AddDbContext<TodoDbContext>(opt =>
 });
 var app = builder.Build();
 
-app.MapGet("/todoitems", () => async (TodoDbContext db) => 
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (TodoDbContext db) =>
     await db.Todos.ToListAsync());
 
-app.MapGet("/todoitems/complete", async (TodoDbContext db) =>  
+todoItems.MapGet("/complete", async (TodoDbContext db) =>  
     await db.Todos.Where(t => t.IsComplete).ToListAsync());
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDbContext db) =>
+todoItems.MapGet("/{id}", async (int id, TodoDbContext db) =>
     await db.Todos.FindAsync(id)
         is Todo todo
             ? Results.Ok(todo)
             : Results.NotFound()
 );
 
-app.MapPost("/todoitems", async (Todo todo, TodoDbContext db) =>
+todoItems.MapPost("/", async (Todo todo, TodoDbContext db) =>
 {
     db.Todos.Add(todo);
     await db.SaveChangesAsync();
 
     return Results.Created($"/todoitems/{todo.Id}", todo);
+});
+
+todoItems.MapPut("/{id}", async (int id, Todo inputTodo, TodoDbContext db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    
+    if (todo is null) return Results.NotFound();
+
+    todo.Name = inputTodo.Name;
+    todo.IsComplete = inputTodo.IsComplete;
+
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+todoItems.MapDelete("/{id}", async (int id, TodoDbContext db) =>
+{
+    if (await db.Todos.FindAsync(id) is Todo todo)
+    {
+        db.Todos.Remove(todo);
+        await db.SaveChangesAsync();
+        return Results.NoContent();
+    }
+    return Results.NotFound();
 });
 
 app.Run();
